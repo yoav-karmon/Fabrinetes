3c334e19c519b2f0ab603a55f4336700d34a3c98
Yoav karmon <ykarmon.mail@gmail.com>
2025-08-03 22:47:18 +0000

    Add tabulate package to Dockerfile; enhance argument printing in compile.tcl and tasks.py

--- source/project_setup/compile.tcl
+++ source/project_setup/compile.tcl
@@ -7,7 +7,7 @@ set debug_probe   [lindex $argv 4]
 set param_string  [lindex $argv 5]
 set define_string [lindex $argv 6]
 
-# Debug: print all arguments
+puts "(i) print all arguments"
 puts "=========== TCL Arguments ==========="
 puts "Project file:     $path_xpr"
 puts "Stage:            $stage"
@@ -18,67 +18,82 @@ puts "Parameters:       $param_string"
 puts "Defines:          $define_string"
 puts "====================================="
 
-# Open project
+puts "(i) Open project"
 set project_file "${path_xpr}"
 open_project $project_file
-# Start with an empty string
-set more_opts ""
-set run_obj [get_runs $synth_run]
+puts "================== stage = synthesis =================="
+
+if { $stage == "syn" | $stage == "all" } {
+    puts "(i) preparing synthesis run OPTIONS"
+    set more_opts ""
+    set run_obj [get_runs $synth_run]
 
 
-if { $define_string ne "" } {
-    foreach define [split $define_string " "] {
-        append more_opts " -verilog_define $define"
+    if { $define_string ne "" } {
+        foreach define [split $define_string " "] {
+            append more_opts " -verilog_define $define"
+        }
     }
-}
 
-if { $param_string ne "" } {
-    foreach param [split $param_string " "] {
-        append more_opts " -generic $param"
+    if { $param_string ne "" } {
+        foreach param [split $param_string " "] {
+            append more_opts " -generic $param"
+        }
     }
-}
+    puts "=================================================="
+    puts "Current MORE_OPTIONS for synthesis run '$synth_run':"
+    puts "$more_opts"
+    puts "=================================================="
 
-# Apply final options to the run
-puts "Setting MORE_OPTIONS for $synth_run: $more_opts"
-set_property -name {STEPS.SYNTH_DESIGN.ARGS.MORE OPTIONS} -value $more_opts -objects $run_obj
-puts "read back MORE_OPTIONS for $synth_run"
-report_property  $run_obj
-puts "=================================================="
-puts "Current MORE_OPTIONS for synthesis run '$synth_run':"
-puts "$more_opts"
-puts "=================================================="
+    puts "(i) Setting MORE_OPTIONS for $synth_run: $more_opts"
+    set_property -name {STEPS.SYNTH_DESIGN.ARGS.MORE OPTIONS} -value $more_opts -objects $run_obj
+    puts "(i) Setting run properties for $synth_run"
+    report_property  $run_obj
+
+    puts "(i) processing stages: $stage"
 
 
 
-puts "================== stage = $stage =================="
-
-# Synthesis
-if { $stage == "syn" } {
     set progress     [get_property PROGRESS [get_runs $synth_run]]
     set need_refresh [get_property NEEDS_REFRESH [get_runs $synth_run]]
     set status       [get_property STATUS [get_runs $synth_run]]
     set status_lower [string tolower $status]
 
-    puts "$synth_run: $status (PROGRESS: $progress), needs_refresh: $need_refresh"
+    puts "(i) $synth_run: $status (PROGRESS: $progress), needs_refresh: $need_refresh"
 
     if { [string match "*complete*" $status_lower] == 0 || $need_refresh == 1 } {
-        puts "Resetting and launching synthesis run: $synth_run"
+        puts "(!) Resetting and launching synthesis run: $synth_run"
         reset_runs $synth_run
         launch_runs $synth_run -to_step synth_design -jobs 4
         wait_on_run [get_runs -filter {IS_SYNTHESIS == TRUE}]
-        open_run $synth_run
-
         if { $debug_probe } {
-            puts "Writing debug probes..."
-            write_debug_probes -force
+            puts "(i) Writing debug probes..."
+            open_run $synth_run
+            write_debug_probes -file debug_probes.ltx -force
         }
     } else {
-        puts "Skipping $synth_run (STATUS: $status)"
+        puts "(!) Skipping $synth_run (STATUS: $status)"
+        if { $debug_probe } {
+            puts "(i) Writing debug probes..."
+            open_run $synth_run
+            write_debug_probes -file debug_probes.ltx -force
+            puts "(i) Writing debug probes completed."
+        }
     }
 
-} 
+} else {
+    set run_obj [get_runs $synth_run]
+    report_property  $run_obj
+    puts "(!) Skipping synthesis stage"
+}
+puts "======================================================"
+puts ""
 
-if { $stage == "impl" } {
+
+puts "================== stage = Implementation ============"
+
+
+if { $stage == "impl" | $stage == "all" } {
     # Implementation
     set progress     [get_property PROGRESS [get_runs $impl_run]]
     set need_refresh [get_property NEEDS_REFRESH [get_runs $impl_run]]
@@ -97,18 +112,23 @@ if { $stage == "impl" } {
     }
 
 } elseif { $stage == "bit" } {
-    # Bitstream
     puts "Writing bitstream for: $impl_run"
     write_bitstream -force [get_runs $impl_run]
     wait_on_run [get_runs -filter {IS_IMPLEMENTATION == TRUE}]
 } else {
-    puts "Unknown stage: $stage"
+    puts "(!) Skipping implementation / bit stage"
 }
 
-# Final status report
+puts "======================================================"
+puts ""
+
+puts "(i) Final status report"
 puts "==================== Final run statuses ===================="
 set status       [get_property STATUS [get_runs $synth_run]]
 puts "$synth_run: $status"
 set status       [get_property STATUS [get_runs $impl_run]]
 puts "$impl_run: $status"
 puts "============================================================"
+
+puts "(i) tcl script completed."
+
