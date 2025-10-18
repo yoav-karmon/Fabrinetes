1d8081822b0cb0bc3ad460edee91dbe57fe90432
yoav karmon <ykarmon.mail@gmail.com>
2025-10-17 20:41:54 -0500

    Update container configuration and environment paths

--- containers/fabrinetes-dev-testing/init_env.sh
+++ containers/fabrinetes-dev-testing/init_env.sh
@@ -1,2 +1,2 @@
-export PATH="/opt/vivado/bin:/opt/project_setup:$HOME/.local/bin:$PATH"
+export PATH="/opt/vivado/bin:$HOME/repo/Fabrinetes/source/project_setup:$HOME/.local/bin:$PATH"
 export XILINXD_LICENSE_FILE="$HOME/repos/phy_project/Xilinx.lic"
